5b41adbfedfbec7aca4cfca4d646b2cf61698b81
fancyflame <fancyflame@163.com>
2025-03-12 13:12:46 +0800

    feat: 添加空白教室列表页面

--- web/src/ClassroomPage/index.tsx
+++ web/src/ClassroomPage/index.tsx
@@ -1,1 +1,15 @@
-export function ClassroomPage() {}
+import { IconChevronLeft } from "@douyinfe/semi-icons";
+import styles from "./index.module.less";
+
+export function ClassroomPage() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.insideContainer}>
+                <div className={styles.title}>
+                    <IconChevronLeft />
+                    教室列表
+                </div>
+            </div>
+        </div>
+    );
+}
